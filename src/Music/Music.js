6bda53530b86802fedfd124d448b301a6657b026
Dranithix <dranithix@gmail.com>
2016-10-23 12:59:11 +0800

    Finished music player & notifications & personalized recommendations.

--- src/Music/Music.js
+++ src/Music/Music.js
@@ -6,6 +6,7 @@ import {Link} from "react-router";
 import "../tile.css";
 import "../flex.css";
 import "./MusicTrack/MusicTrack";
+import request from "browser-request";
 
 function listToMatrix(list, elementsPerSubArray) {
     var matrix = [], i, k;
@@ -24,25 +25,45 @@ function listToMatrix(list, elementsPerSubArray) {
 
 Music({
     tracks: [],
+    profile: null,
     created() {
-      let urls = {
-        hiphop: "https://soundcloud.com/alltrapnation",
-        rock: "https://soundcloud.com/killrockstars",
-        classical: "https://soundcloud.com/classicalkingfm"
-      };
+        let urls = {
+            hip_hop: "https://soundcloud.com/alltrapnation",
+            rock: "https://soundcloud.com/killrockstars",
+            classical: "https://soundcloud.com/classicalkingfm"
+        };
+        let self = this;
 
-      let self = this;
+        request.get({
+            url: "http://watson-seamless.herokuapp.com/realDonaldTrump", json: true
+        }, (req, res, data) => {
+            self.profile(data);
 
-      SC.resolve(urls.hiphop).then(data => {
-        return data.id
-      }).then(id => {
-        return SC.get(`/users/${id}/tracks`);
-      }).then(tracks => {
-        tracks.forEach(track => {
-          console.log(track);
-          self.tracks().push({id: track.id, title: track.title, artwork: track.artwork_url, stream: track.stream_url + "?client_id=2b7b69947b17fa8e085f79616c35dafc"})
+            let max = "";
+            for (var key in data.ranks_music) {
+                if (data.ranks_music[key] == 1) {
+                    max = key;
+                    break;
+                }
+            }
+
+            SC.resolve(urls[max]).then(data => {
+                return data.id
+            }).then(id => {
+                return SC.get(`/users/${id}/tracks`);
+            }).then(tracks => {
+                tracks.forEach(track => {
+                    self.tracks().push({
+                        id: track.id,
+                        title: track.title,
+                        artwork: track.artwork_url,
+                        stream: track.stream_url + "?client_id=2b7b69947b17fa8e085f79616c35dafc"
+                    })
+                })
+            })
         })
-      })
+
+
     },
     destroyed() {
     },
@@ -88,9 +109,30 @@ Music({
         this.initTiles();
 
         $("body").children().children().css("height", "100%");
+
+        setTimeout(() => {
+            $.Notify({
+                caption: 'Lunch',
+                content: '<p style="font-size: 24px;">Jainam, your vegetarian meal will be served in 10 minutes!</p>',
+                icon: "<span class='mif-vpn-publ'></span>",
+                type: "warning",
+                timeout: 10000
+            });
+        }, 6000);
+
+        setTimeout(() => {
+            $.Notify({
+                caption: 'Music',
+                content: `<p style="font-size: 24px;">You're now listening to: The Lifted - Mr. Sandman (ft. Ashliann)</p>`,
+                icon: "<span class='mif-vpn-publ'></span>",
+                type: "info",
+                timeout: 5000
+            });
+        }, 2000);
+
     },
     initTiles() {
-      var tiles = $(".tile, .tile-small, .tile-square, .tile-wide, .tile-large, .tile-big, .tile-super");
+        var tiles = $(".tile, .tile-small, .tile-square, .tile-wide, .tile-large, .tile-big, .tile-super");
 
         $.each(tiles, function () {
             var tile = $(this);
@@ -126,46 +168,39 @@ Music({
             </div>
 
             <div class="tile-group double">
-                <span class="tile-group-title"></span>
-
-                <div class="tile-container">
-
-                    <a href="#" class="tile-wide bg-darkBlue fg-white">
-                        <div class="tile-content iconic">
-                            <span class="icon mif-cloudy"></span>
-                        </div>
-                        <span class="tile-label">Altitude: 40,000ft.</span>
-                    </a>
-
-                    <div class="tile-wide bg-blue fg-white">
-                        <div class="tile-content iconic">
-                            <span class="icon mif-thermometer"></span>
-                        </div>
-                        <span class="tile-label">Temperature: -51°C</span>
+                <span class="tile-group-title">
+                    <div data-role="audio" style="width: 605px;">
+                        <audio id="audioPlayer">
+                            <source src="http://metroui.org.ua/res/rec.mp3" type='audio/mp3'/>
+                        </audio>
                     </div>
+                </span>
 
-                    <div class="tile-wide bg-teal fg-white">
-                        <div class="tile-content iconic">
-                            <span class="icon mif-space-shuttle mif-4x"></span>
+                <div class="tile-container" style="margin-top: 32px;">
+                    <div class="tile tile-big-x bg-teal fg-white">
+                        <div class="tile-content">
+                            <span class="tile-label" style="padding: 16px; text-align: center;">Hey Kenta! We looked through your music and thought that <b>Hip Hop</b> suits you. <br/><br/>Here's some of the latest tracks from SoundCloud :).<br/><br/></span>
                         </div>
-                        <span class="tile-label">Time to Destination: (2:22)</span>
                     </div>
+                    <br/><br/><br/><br/>
+
+
                 </div>
             </div>
 
             {
-              listToMatrix(this.tracks(), 6).map((r, i) => {
-                return <div class="tile-group double" key={i}>
-                  <span class="tile-group-title"></span>
-                  <div class="tile-container">
-                    {
-                      r.map((p, n) => {
-                          return <MusicTrack {...p} key={n} />
-                      })
-                    }
-                  </div>
-                </div>
-            })}
+                listToMatrix(this.tracks(), 6).map((r, i) => {
+                    return <div class="tile-group double" key={i}>
+                        <span class="tile-group-title"></span>
+                        <div class="tile-container" style="margin-top: 32px;">
+                            {
+                                r.map((p, n) => {
+                                    return <MusicTrack {...p} key={n}/>
+                                })
+                            }
+                        </div>
+                    </div>
+                })}
         </div>
     }
 })
--- src/Music/MusicTrack/MusicTrack.js
+++ src/Music/MusicTrack/MusicTrack.js
@@ -1,35 +1,42 @@
 MusicTrack({
-  artwork: '',
-  stream: '',
-  id: '',
-  title: '',
-  playTrack() {
-    console.log(this.artwork());
-  },
-  rendered() {
-    let self = this;
-      setTimeout(() => {
-        $(`#${self.id()}`).css({
-          opacity: 1,
-          "-webkit-transform": "scale(1)",
-          "transform": "scale(1)",
-          "-webkit-transition": ".3s",
-          "transition": ".3s"
-      })
-      }, Math.floor(Math.random() * 500));
-  },
-  render() {
-    <div id={this.id()} class="tile bg-blue fg-white" b="click: playTrack">
-      <div class="tile-content">
-        <div class="image-container">
-            <div class="frame">
-              <img src={this.artwork()} />
-            </div>
-            <div class="image-overlay">
-                {this.title()}
+    artwork: '',
+    stream: '',
+    id: '',
+    title: '',
+    playTrack() {
+        console.log(this.stream());
+
+        let audio = $("#audioPlayer");
+        audio.attr("src", this.stream());
+        audio[0].pause();
+        audio[0].load();
+        audio[0].oncanplaythrough = audio[0].play();
+    },
+    rendered() {
+        let self = this;
+        setTimeout(() => {
+            $(`#${self.id()}`).css({
+                opacity: 1,
+                "-webkit-transform": "scale(1)",
+                "transform": "scale(1)",
+                "-webkit-transition": ".3s",
+                "transition": ".3s"
+            })
+        }, Math.floor(Math.random() * 500));
+        $.StartScreen();
+    },
+    render() {
+        <div id={this.id()} class="tile bg-blue fg-white" b="click: playTrack">
+            <div class="tile-content">
+                <div class="image-container">
+                    <div class="frame">
+                        <img src={this.artwork()}/>
+                    </div>
+                    <div class="image-overlay">
+                        {this.title()}
+                    </div>
+                </div>
             </div>
         </div>
-      </div>
-  </div>
-  }
+    }
 });
